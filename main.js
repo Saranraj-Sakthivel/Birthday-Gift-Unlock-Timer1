c0de2a843a80453151415f1b942c79cf06b9b807
Saranraj-Sakthivel <sriramsaran95@gmail.com>
2025-12-09 21:56:56 +0530

    time

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ days.innerText=d;
 hours.innerHTML=h;
 minutes.innerHTML=m;
 seconds.innerHTML =s;
-    if(d===0 && h===2 && m==7 && s===0)
+    if(d===0 && h===2 && m==0 && s===0)
     {
         demo()
     }
